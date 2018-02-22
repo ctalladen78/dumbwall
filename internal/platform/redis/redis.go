599b9443e27a078b450dce42b3900f4e9da7301e
Maksadbek <a.maksadbek@gmail.com>
2018-02-22 20:43:53 +0300

    platform/redis: added base functions to get/set listings operations to add and get best/hot/top/rising/controversial listings.

--- internal/platform/redis/redis.go
+++ internal/platform/redis/redis.go
@@ -10,59 +10,76 @@ type Redis struct {
 	pool *redigo.Pool
 }
 
-func New() *Redis {
-	return &Redis{}
+func New(addr string, maxIdle, maxActive, idleTimeout int) *Redis {
+	return &Redis{
+		pool: &redigo.Pool{
+			MaxIdle:     maxIdle,
+			MaxActive:   maxActive,
+			IdleTimeout: time.Second * time.Duration(idleTimeout),
+			Dial:        func() (redigo.Conn, error) { return redigo.Dial("tcp", addr) },
+		},
+	}
 }
 
 func (r *Redis) Hot(b, e uint64) ([]string, error) {
-	c := r.pool.Get()
-	defer c.Close()
-
-	c.
-
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("hot", b, e))
 }
 
-func (r *Redis) PutHot(id string, score int) error {
-	return nil
+func (r *Redis) PutHot(id string, score uint64) error {
+	return r.zadd("hot", id, score)
 }
 
 func (r *Redis) Best(b, e uint64) ([]string, error) {
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("best", b, e))
 }
 
-func (r *Redis) PutBest(id string, score int) error {
-	return nil
+func (r *Redis) PutBest(id string, score uint64) error {
+	return r.zadd("best", id, score)
 }
 
 func (r *Redis) Top(b, e uint64) ([]string, error) {
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("top", b, e))
 }
 
-func (r *Redis) PutTop(id string, score int) error {
-	return nil
+func (r *Redis) PutTop(id string, score uint64) error {
+	return r.zadd("put", id, score)
 }
 
 func (r *Redis) Controversial(b, e uint64) ([]string, error) {
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("controversial", b, e))
 }
 
-func (r *Redis) PutControversial(id string, score int) error {
-	return nil
+func (r *Redis) PutControversial(id string, score uint64) error {
+	return r.zadd("controversial", id, score)
 }
 
 func (r *Redis) New(b, e uint64) ([]string, error) {
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("new", b, e))
 }
 
-func (r *Redis) PutNew(id string, createdAt int32) error {
-	return nil
+func (r *Redis) PutNew(id string, createdAt int64) error {
+	return r.zadd("new", id, uint64(createdAt))
 }
 
 func (r *Redis) Rising(b, e uint64) ([]string, error) {
-	return []string{}, nil
+	return redigo.Strings(r.zrevrange("rising", b, e))
 }
 
-func (r *Redis) PutRising(id string, score int32) error {
-	return nil
+func (r *Redis) PutRising(id string, score uint64) error {
+	return r.zadd("rising", id, score)
+}
+
+func (r *Redis) zrevrange(key string, b, e uint64) (reply interface{}, err error) {
+	c := r.pool.Get()
+	defer c.Close()
+
+	return redigo.Strings(c.Do("ZREVRANGE", key, b, e))
+}
+
+func (r *Redis) zadd(key string, member string, score uint64) error {
+	c := r.pool.Get()
+	defer c.Close()
+
+	_, err := c.Do("ZADD", key, score, member)
+	return err
 }
