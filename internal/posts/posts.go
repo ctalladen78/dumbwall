b3a1d78641ecf1780ebff24504b1ae2a7d0627c4
Maksadbek <a.maksadbek@gmail.com>
2018-04-08 22:03:14 +0300

    show elapsed time after post created in human readable form

--- internal/posts/posts.go
+++ internal/posts/posts.go
@@ -1,6 +1,7 @@
 package posts
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/maksadbek/dumbwall/internal/actions"
@@ -40,4 +41,29 @@ type Meta struct {
 
 func (p Post) Validate() []error {
 	return nil
+}
+
+func (p Post) FormattedCreatedAt() string {
+	sub := time.Now().Sub(p.CreatedAt)
+
+	if sub.Minutes() < 1 {
+		if sub.Seconds() < 1 {
+			return "a moment ago"
+		}
+		return fmt.Sprintf("%.0f seconds ago", sub.Seconds())
+	}
+
+	if sub.Minutes() < 60 {
+		return fmt.Sprintf("%.0f minutes ago", sub.Minutes())
+	}
+
+	if sub.Hours() < 24 {
+		return fmt.Sprintf("%.0f hours ago", sub.Hours())
+	}
+
+	if sub.Hours() < 48 {
+		return "yesterday"
+	}
+
+	return p.CreatedAt.Format("Mar 7 2015")
 }
